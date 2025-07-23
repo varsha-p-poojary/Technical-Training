fdbb3bc8727786dc69d751fea226e52fe30ebe67
varsha-p-poojary <varshapoojary.su@gmail.com>
2025-07-23 11:46:40 +0530

    Create printing_values.c

---
+++ printing_values.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a = 20;
+    float b = 15.36;
+    char c = 'A';
+    printf("%d\n",a);
+    printf("%f\n",b);
+    printf("%c\n",c);
+    printf("%d\n",sizeof(a));
+    printf("%d\n",sizeof(b));
+    printf("%d\n",sizeof(c));
+    return 0;
+}
