af5a7466bda15a97f017466078507bb86452d6d4
varsha-p-poojary <varshapoojary.su@gmail.com>
2025-07-23 12:05:53 +0530

    Create positive_number.c

---
+++ positive_number.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("Enter a number: ");
+    scanf("%d",&a);
+    if(a>0)
+    {
+        printf("The given number is positive");
+        printf("\nNumber = %d",a);
+    }
+    return 0; 
+}
