4ad85d1a2f42bdf80f71f67044f16bb8145822bb
varsha-p-poojary <varshapoojary.su@gmail.com>
2025-07-24 10:40:44 +0530

    Create to print even natural numbers.c

---
+++ to print even natural numbers.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int n,i=1;
+    printf("Enter a number: ");
+    scanf("%d",&n);
+    while(i<=n)                            //to print even natural numbers
+    {
+        if(i%2==0)
+        printf("%d\n",i);
+        i++;
+    }
+    return 0; 
+        s++;
+    }
+return 0; 
+}
+
+
