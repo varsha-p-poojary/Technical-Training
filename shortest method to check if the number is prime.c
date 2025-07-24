c928dd4670400e83d4590f9b1a5489373e13a218
varsha-p-poojary <varshapoojary.su@gmail.com>
2025-07-24 11:36:15 +0530

    Create shortest method to check if the number is prime.c

---
+++ shortest method to check if the number is prime.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+int n, i, count=0;
+    printf("Enter a number ");         
+    scanf("%d",&n);
+    for(i=2;i<=sqrt(n);i++)                      //shortest method to check if the number is prime
+    {
+        if(n%i==0)
+        count++;
+    }
+    if(count==0)
+    printf("Prime");
+    else
+    printf("Not a prime");
+    return 0; 
+    }
