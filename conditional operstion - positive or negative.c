d569040fb1614c4bddd5f6df36466864dded77f8
varsha-p-poojary <varshapoojary.su@gmail.com>
2025-07-27 22:28:11 +0530

    Create conditional operation - positive or negative.c

---
+++ conditional operstion - positive or negative.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int a;                                           //conditional operstion - positive or negative
+    printf("Enter a number : ");
+    scanf("%f",&a);
+    (a>0)? printf("Positive"):printf("Negative");
+    return 0; 
+}
