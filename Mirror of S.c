63a1e6b9efe17b252c9932d51c336665daeec23d
varsha-p-poojary <varshapoojary.su@gmail.com>
2025-07-27 22:42:32 +0530

    Create Mirror of S.c

---
+++ Mirror of S.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+int main()
+{
+    int i, j, n;
+    printf("Enter the row");
+    scanf("%d", &n);
+    for(i = 1; i <= n; i++)
+    {
+        if(i == 1 || i == n || i == (n + 1) / 2)
+        {
+            for(j = 1; j <= n; j++)
+                printf("* ");
+        }
+        else if(i < (n + 1) / 2)
+        {
+            for(j = 1; j <= n; j++)
+            {
+                if(j == n)
+                    printf("* ");
+                else
+                    printf("  ");
+            }
+        }
+        else
+        {
+            for(j = 1; j <= n; j++)
+            {
+                if(j == 1)
+                    printf("* ");
+                else
+                    printf("  ");
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
