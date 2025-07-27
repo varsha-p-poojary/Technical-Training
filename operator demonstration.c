a2671ce1716f00d515da1b0367a9c5fc4efc00c2
varsha-p-poojary <varshapoojary.su@gmail.com>
2025-07-27 22:29:25 +0530

    Create operator demonstration.c

---
+++ operator demonstration.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int a= 278, b=353;                              //operator demonstration
+    printf("%d",a&b);
+    printf("\n%d",a|b);
+    printf("\n%d",a^b);
+    printf("\n%d",a<<2);
+    printf("\n%d",a>>2);
+    return 0; 
+}
