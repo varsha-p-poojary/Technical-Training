d8fc9f6e0c4ac37d373429b0bcd2b9ca7186da2d
varsha-p-poojary <varshapoojary.su@gmail.com>
2025-07-27 22:21:42 +0530

    Create to find the square root.c

---
+++ to find the square root.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+   float a;
+    printf("Enter the number whose square root has to be found: ");
+    scanf("%f",&a);
+    printf("The square of %f is %f",a,sqrt(a)); 
+return 0;
+}
