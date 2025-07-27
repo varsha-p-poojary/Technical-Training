367c244bc7746693f28c68c252ee798949e0f2ed
varsha-p-poojary <varshapoojary.su@gmail.com>
2025-07-27 22:39:55 +0530

    Create Character Triangle A, AB, ABC.c

---
+++ Character Triangle A, AB, ABC.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int i, j, n;
+    char c;
+    printf("Enter the row");
+    scanf("%d", &n);
+    for(i = 1; i <= n; i++)
+    {
+        c = 'A';
+        for(j = 1; j <= i; j++)
+        {
+            printf("%c ", c);
+            c++;
+        }
+        printf("\n");
+    }
+    return 0;
+}
